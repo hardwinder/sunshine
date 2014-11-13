24e0882c700d2e9effb9e94d40169ad44a1db6f4
Harwin Prahara <hardwinder@gmail.com>
2014-11-13 11:32:20 +0700

    Finished lesson 2, connected to cloud

---
+++ app/src/main/java/ckpd/sunshine/WeatherDataParser.java
@@ -0,0 +1,28 @@
+package ckpd.sunshine;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class WeatherDataParser {
+
+    /**
+     * Given a string of the form returned by the api call:
+     * http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7
+     * retrieve the maximum temperature for the day indicated by dayIndex
+     * (Note: 0-indexed, so 0 would refer to the first day).
+     */
+    public static double getMaxTemperatureForDay(String weatherJsonStr, int dayIndex)
+            throws JSONException {
+        JSONObject weather = new JSONObject(weatherJsonStr);
+        JSONArray days = weather.getJSONArray("list");
+        JSONObject dayInfo = days.getJSONObject(dayIndex);
+        JSONObject temperatureInfo = dayInfo.getJSONObject("temp");
+        return temperatureInfo.getDouble("max");
+    }
+
+
+
+}
